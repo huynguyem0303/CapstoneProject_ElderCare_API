04c61b10bcc40633fcbe06be9306c2aec18b3fec
Hue <huetqse161477@fpt.edu.vn>
2024-05-02 21:13:46 +0700

    add model

---
+++ ElderCare_Domain/Models/TrackingOption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElderCare_Domain.Models
+{
+    public class TrackingOption
+    {
+        public int TrackingOptionId { get; set; }
+        public int ServiceId { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+    }
+}
